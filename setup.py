eba490e8111113a02882f95a7dcf5a8f9323a955
Roy Hyunjin Han <rhh@crosscompute.com>
2020-05-23 01:53:40 -0400

    Include MANIFEST

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
 
 setup(
     name='invisibleroads-macros-database',
-    version='1.0.1',
+    version='1.0.2',
     description='Shortcut functions for database operations',
     long_description=DESCRIPTION,
     long_description_content_type='text/markdown',
